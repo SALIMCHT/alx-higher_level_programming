4c284f07f5cd7a4328dd4299df11071394887a1c
SALIMCHT <saliimchatbii@gmail.com>
2024-04-04 19:22:44 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-a = type(html)
-b = html.decode("utf-8")
-print("Body response:")
-print("\t- type: {}".format(a))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(b))
+if __name__ == "__main__":
+    Url0 = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(Url0)
+
+    with urllib.request.urlopen(req) as response:
+        # Displaying information about the response body
+        res = response.read()
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf-8")))
