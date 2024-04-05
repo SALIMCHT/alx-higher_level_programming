34a143e227c4d595d2c01be8d248da39447895ce
SALIMCHT <saliimchatbii@gmail.com>
2024-04-04 19:26:01 -0700

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" With request"""
+
+"""fetches https://alx-intranet.hbtn.io/status using requests """
+
 import requests
-html = requests.get('https://intranet.hbtn.io/status')
-a = type(html.text)
-print("Body response:")
-print("\t- type: {}".format(a))
-print("\t- content: {}".format(html.text))
+
+if __name__ == "__main__":
+    request = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(request.text)))
+    print("\t- content: {}".format(request.text)))
